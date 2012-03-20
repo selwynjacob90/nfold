3c799ef519f99fa8b9f999d282fbd8349c6d2488
Selwyn Jacob <selwynjacob90@gmail.com>
2012-03-20 20:53:46 +0530

    Create css for the app.

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+import os
 
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,5 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(SITE_ROOT, 'content')}),
 
 )
