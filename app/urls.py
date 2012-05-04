404ad3a72399a428c80c3f69e7a70788fd04aebc
Selwyn Jacob <selwynjacob90@gmail.com>
2012-05-04 11:31:23 +0530

    Add URLs for the new views.

--- app/urls.py
+++ app/urls.py
@@ -1,19 +1,34 @@
+""" This module contains the app specific URLs
+"""
+
+__author__ = 'Selwyn Jacob <selwynjacob@gmail.com>' 
+
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.list_detail import object_list
 
-from app.views import RnaInput, DisplayRepresentations, Home 
+from app.models import Sequence
+from app.views import input_page, seq_process, home, list_page, ref_page
 
+seq_info = {'queryset' : Sequence.objects.all }
 
 urlpatterns = patterns('',
                        url(r'^$',
-                           Home,
+                           home,
                            name = 'homepage'),
+                       
+                       url(r'^(?P<object_id>\d+)/$',
+                           seq_process,
+                           name = 'seq_detail'),
 
                        url(r'^new/$',
-                           RnaInput,
-                           name = 'input_page'), 
-
-                       url(r'^(?P<object_id>\d+)/$',
-                           DisplayRepresentations,
-                           name = 'display_rep'),
-
+                           input_page,
+                           name = 'input_page'),
+           
+                       url(r'^list/$',
+                           list_page,
+                           name = 'list_page'), 
+             
+                       url(r'^references/$',
+                           ref_page,
+                           name = 'reference_page'),
                       )
